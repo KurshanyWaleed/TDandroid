217e2d863ebd7174d908dc7d714e3fe0e221afb5
kurshanyWaleed <kurshany.waleed@gmail.com>
2020-10-30 12:46:53 +0100

    just test

---
+++ app/src/main/java/com/example/tdcours/Activity_send_sms.java
@@ -0,0 +1,14 @@
+package com.example.tdcours;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_send_sms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_sms);
+    }
+}
